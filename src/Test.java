9d799e9f023250b20bea9f5be8bae58ae86bcd79
00912yuan <you@example.com>
2020-12-26 23:22:49 +0800

    读取Excel中的内容

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,6 @@
-import java.util.Scanner;
 
+import java.io.File;
+import java.util.Scanner;
 public class Test {
     public  static void main(String[] args){
         System.out.println("请输入用户名：");
@@ -9,5 +10,16 @@ public class Test {
         System.out.println("请输入密码：");
         String password = sc.next();
         System.out.println("请输入密码："+password);
+        File file =new File("C:\\Users\\ASUS\\Desktop\\consolseshop\\src\\User.xlsx");
+        ReadExcel readExcel = new ReadExcel();
+        User users[]=readExcel.readExcel(file);
+        for(int i=0;i<users.length;i++){
+            if (Username.equals(users[i].getUsername()) &&password.equals(users[i].getPassword())){
+                System.out.println("登陆成功");
+                break;
+            } else {
+                System.out.println("登录失败");
+            }
+        }
     }
 }
---
+++ src/User.java
@@ -0,0 +1,41 @@
+public class User {
+    private String Username;
+    private String password;
+    private String address;
+    private String phone;
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void getpassword() {
+    }
+}
