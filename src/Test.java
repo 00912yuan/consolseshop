af2deb8b42397a7aa8c60bc83a1a254fb7ff47ea
ＡＡ <1141783045@QQ.COM>
2020-12-26 16:54:35 +0800

    接受键盘输入用户名和密码

--- src/Test.java
+++ src/Test.java
@@ -4,7 +4,7 @@ public class Test {
     public  static void main(String[] args){
         System.out.println("请输入用户名：");
         Scanner sc = new Scanner(System.in);
-        String Username =sc.next();
+       String Username =sc.next();
         System.out.println("请输入用户名："+Username);
         System.out.println("请输入密码：");
         String password = sc.next();
